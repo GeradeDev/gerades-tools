9c40070e9b0e3706672e1ff47346a0ad0f5cea2d
Gerade Geldenhuys <GeradeGeldenhuys@Gmail.com>
2018-09-14 16:51:50 +0200

    -initial commit

---
+++ gerades-tools/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reflection;
+
+namespace gerades_tools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                var versionString = Assembly.GetEntryAssembly()
+                                        .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+                                        .InformationalVersion
+                                        .ToString();
+
+                Console.WriteLine($"gerade tools v{versionString}");
+                Console.WriteLine("-------------");
+                return;
+            }
+
+            StartMyTools(string.Join(' ', args));
+        }
+
+        public static void StartMyTools(string args)
+        {
+            Console.WriteLine("Off we go");
+        }
+    }
+}
